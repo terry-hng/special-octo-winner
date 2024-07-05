a91c53726d0a30523966674c817e17682cbb4279
Terry Nguyen <40750175+terry-hng@users.noreply.github.com>
2024-07-05 13:31:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -23,13 +23,13 @@ percent_changed = float(price_data["priceChangePercent"])
 message = f"> **Daily NEAR Price Report** - {dt.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh')).strftime("%A, %B %d")}\n\n"
 
 if percent_changed > 0 and abs(percent_changed) > 5: 
-    message += f"🟩   **+{percent_changed}%**\t|\t**+${amount_changed}**   ❗❗❗\n\n- Current price: **${last_price}**\n\n- Yesterday's price: **${previous_close_price}**"
+    message += f"🟩   **+{percent_changed}%**\t|\t**+${amount_changed}**   ❗❗❗\n\n- Current price: **${last_price}**\n\n- Yesterday's close: **${previous_close_price}**"
 elif percent_changed < 0 and abs(percent_changed) > 5: 
-    message += f"🟥   **{percent_changed}%\t|\t-${abs(amount_changed)}**   ❗❗❗\n\n- Current price: **${last_price}**\n\n- Yesterday's price: **${previous_close_price}**"
+    message += f"🟥   **{percent_changed}%\t|\t-${abs(amount_changed)}**   ❗❗❗\n\n- Current price: **${last_price}**\n\n- Yesterday's close: **${previous_close_price}**"
 elif percent_changed > 0:
-    message += f"🟩   **+{percent_changed}%**\t|\t**+${amount_changed}**\n\n- Current price: **${last_price}**\n\n- Yesterday's price: **${previous_close_price}**"
+    message += f"🟩   **+{percent_changed}%**\t|\t**+${amount_changed}**\n\n- Current price: **${last_price}**\n\n- Yesterday's close: **${previous_close_price}**"
 elif percent_changed < 0:
-    message += f"🟥   **{percent_changed}%\t|\t-${abs(amount_changed)}**\n\n- Current price: **${last_price}**\n\n- Yesterday's price: **${previous_close_price}**"
+    message += f"🟥   **{percent_changed}%\t|\t-${abs(amount_changed)}**\n\n- Current price: **${last_price}**\n\n- Yesterday's close: **${previous_close_price}**"
 else:
     message += f"⬜   **{int(percent_changed)}%**"
 
